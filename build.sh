87be7309dd0a46babc92c35af80ccf64c1950fa8
Banishdream <67967198+Banishdream@users.noreply.github.com>
2023-04-17 09:42:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -trimpath -o ./
 echo "build windows x64 cvemonitor..."
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -trimpath -o ./cvemonitor_windows_x64.exe ./main.go
 echo "build linux x64 cvemonitor..."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -trimpath -o ./cvemonitor_linux_x64 ./cvemonitor.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -trimpath -o ./cvemonitor_linux_x64 ./main.go
 echo "build macos arm64 cvemonitor..."
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -trimpath -o ./cvemonitor_macos_arm64 ./cvemonitor
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -trimpath -o ./cvemonitor_macos_arm64 ./main.go
